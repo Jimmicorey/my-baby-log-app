0f4699edd7beecaf980b9182897cde7782a4a088
James C. Coffelt <james.c.coffelt@gmail.com>
2020-07-31 12:23:32 -0500

    added folder structure

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  API_ENDPOINT: 'http://localhost:8000/api/datalogs', //<--- may have to change this???
+  
+  //TOKEN_KEY: 'my-baby-log-auth-token', <<---- STRETCH GOAL --->>
+}
+
+
+/** "MY BABY LOG" CURRENT USABLE ENDPOINTS 
+ * /                   <--- "Hello, Mommies & Daddies!" --->
+ * /allthedata         <--- ALL the datalogs --->
+ * /feedbabydata       <--- "Feed Baby" datalogs --->
+ * /changediaperdata   <--- "Change Diaper" datalogs --->
+ * /bathtimedata       <--- "Bath Time" datalogs --->
+ *  
+**/ 
