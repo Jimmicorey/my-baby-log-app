c69e66922ed0a432e5a48aad81445a298d8c4885
James Corey Coffelt <james.c.coffelt@gmail.com>
2020-08-01 19:33:19 -0500

    datalogs GET logic works with placeholder API

--- src/components/ChangeDiaper/ChangeDiaperLog.js
+++ src/components/ChangeDiaper/ChangeDiaperLog.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+//import DatalogsApiService from '../../services/datalogs-api-service';
 import './ChangeDiaperLog.css';
 
-/** ChangeDiaperLog should GET (fetch) DATALOGS from API(database) **/
-/** ChangeDiaperLog should render LIST (array) of DATALOGS from API(database) 
- *  ChangeDiaperLog should render 10 MOST RECENT logs **/
+/** 
+ * ChangeDiaperLog should GET (fetch) DATALOGS from API(database)
+ * ChangeDiaperLog should render LIST (array) of DATALOGS from API(database) 
+ * ChangeDiaperLog should render 10 MOST RECENT logs 
+**/
 
 
 class ChangeDiaperLog extends React.Component {
 
-
   /* DUMMY DATA PLACEHOLDER CODE TO SHOW FUNCTIONING GET REQ *////////////////////////
-  state = {
-    loading: true,
-    person: null
-  }
-  /* //////////////////////////////////////////////////////////////////////////// */
-
-  async componentDidMount() {
+  componentDidMount() {
     console.log('FeedBabyLog componentDidMount works!');
 
-    /* DUMMY DATA PLACEHOLDER CODE TO SHOW FUNCTIONING GET REQ */
-    const url = "https://api.randomuser.me/";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
-    /* /////////////////////////////////////////////////////////////////////////// */
+    function createNode(element) {
+      return document.createElement(element);
+    }
 
+    function append(parent, el) {
+      return parent.appendChild(el);
+    }
+
+    const ul = document.getElementById('authors');
+    const url = 'https://randomuser.me/api/?results=10';
+    fetch(url)
+    .then((resp) => resp.json()) //change the data into json
+    .then( function(data) {
+      let authors = data.results; //get the results
+      return authors.map(function(author) { 
+        //map results run the following code
+        //create and append the <li></li>'s to the <ul></ul>
+        let li = createNode('li'),
+            span = createNode('span');
+        span.innerHTML = `${author.name.first} ${author.name.last}`;
+        append(li, span);
+        append(ul, li);
+      })    
+    })
+    .catch( function(error) {
+      //code to handle returned 'error'
+      console.log(error);
+    });
   }
-
+  ///////////////////////////////////////////////////////////////////////////////////
 
   render() {
     return (
@@ -38,21 +55,13 @@ class ChangeDiaperLog extends React.Component {
     
         <div className='flex-container'>
 
-
-          {/* DUMMY DATA PLACEHOLDER CODE TO SHOW FUNCTIONING GET REQ //////////////////*/}
+          {/* DUMMY DATA PLACEHOLDER CODE TO SHOW FUNCTIONING GET REQ //////////////*/}
           <div>
-            {this.state.loading || !this.state.person ? (
-              <div>loading...</div>
-            ) : (
-              <div>
-                <div>{this.state.person.name.title}</div>
-                <div>{this.state.person.name.first}</div>
-                <div>{this.state.person.name.last}</div>
-              </div>
-            )}
+
+            <ul id="authors"></ul>
+
           </div>
           {/* ///////////////////////////////////////////////////////////////////// */}
-
 
 
           <div className='flex-container-row'>
--- src/services/datalogs-api-service.js
+++ src/services/datalogs-api-service.js
@@ -2,79 +2,69 @@ import config from '../config';
 
 const DatalogsApiService = {
 
-//GET ALL THE DATA LOGS     
-getAllDatalogs() {
-    return fetch(`${config.API_ENDPOINT}/allthedata`, {
-        headers: {
-        },
-    })
+    //GET ALL THE DATA LOGS - ENDPOINT TEST
+    getAllDatalogs() {
+        return fetch(`${config.API_ENDPOINT}/allthedata`, {
+            headers: {
+            },
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
+
+
+    //GET FEED BABY DATA LOGS - ENDPOINT 1-1
+    getFeedBabyDatalogs() {
+        return fetch(`${config.API_ENDPOINT}/feedbabydata`, {
+            headers: {
+            },
+        })
         .then(res =>
             (!res.ok)
                 ? res.json().then(e => Promise.reject(e))
                 : res.json()
         )
-},
+    },
 
 
-//GET FEED BABY DATA LOGS
-getFeedBabyDatalogs() {
-    return fetch(`${config.API_ENDPOINT}/feedbabydata`, {
-        headers: {
-        },
-    })
-    .then(res =>
-        (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res.json()
-    )
-},
+
+    //GET CHANGE DIAPER DATA LOGS - ENDPOINT 2-1
+    getChangeDiaperDatalogs() {
+        return fetch(`${config.API_ENDPOINT}/changediaperdata`, {
+            headers: {
+            },
+        })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+    },
 
 
-//GET CHANGE DIAPER DATA LOGS 
-getChangeDiaperDatalogs() {
-    return fetch(`${config.API_ENDPOINT}/changediaperdata`, {
-        headers: {
-        },
-    })
-    .then(res =>
-        (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res.json()
-    )
-},
-
-
-//GET CHANGE DIAPER DATA LOGS 
-getChangeDiaperDatalogs() {
-    return fetch(`${config.API_ENDPOINT}/changediaperdata`, {
-        headers: {
-        },
-    })
-    .then(res =>
-        (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res.json()
-    )
-},
-
-
-//GET BATH TIME DATA LOGS
-getBathTimeDatalogs() {
-    return fetch(`${config.API_ENDPOINT}/bathtimedata`, {
-        headers: {
-        },
-    })
-    .then(res =>
-        (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res.json()
-    )
-},
-
-
+    //GET BATH TIME DATA LOGS  - ENDPOINT 3-1
+    getBathTimeDatalogs() {
+        return fetch(`${config.API_ENDPOINT}/bathtimedata`, {
+            headers: {
+            },
+        })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+    },
 }
 
-export default ;
+export default DatalogsApiService;
+
+
+
+
+
 
 
 /** "MY BABY LOG" CURRENT USABLE ENDPOINTS 
