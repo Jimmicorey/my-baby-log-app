d3db8159c7b991870e7e3510703429a08eeae67c
James C. Coffelt <james.c.coffelt@gmail.com>
2020-08-04 18:51:16 -0500

    updates

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   //API_ENDPOINT: 'http://localhost:8000/api/datalogs', 
 
   //LIVE SERVER AT THE FOLLOWING
-  API_ENDPOINT: `https://my-baby-log-api.herokuapp.com/`,
+  API_ENDPOINT: `https://my-baby-log-api.herokuapp.com/api/`,
  
   
   //TOKEN_KEY: 'my-baby-log-auth-token', <<---- STRETCH GOAL --->>
