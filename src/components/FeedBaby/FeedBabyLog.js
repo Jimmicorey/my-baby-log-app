52f0983477d06700e85ca3f7dcdbd61c598933e1
James Corey Coffelt <james.c.coffelt@gmail.com>
2020-08-04 22:41:27 -0500

    UGGHHHH

--- src/components/FeedBaby/FeedBabyLog.js
+++ src/components/FeedBaby/FeedBabyLog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './FeedBabyLog.css';
+import config from '../config';
 
 /**
  * FeedBabyLog should render 10 MOST RECENT logs
