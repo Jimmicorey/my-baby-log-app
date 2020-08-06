39caf859cc80da97ad9263c7692769f008a6a1a2
James C. Coffelt <james.c.coffelt@gmail.com>
2020-08-06 11:59:49 -0500

    CURRENTLY have running CLOCK.. but not TIMER...ugh

--- src/components/HomePage/FeedBabyTimer.js
+++ src/components/HomePage/FeedBabyTimer.js
@@ -1,56 +1,64 @@
 import React from 'react';
-import config from '../../config';
+//import config from '../../config';
 
 
 class FeedBabyTimer extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            date_created: ""
+            // date_created: "",
+            date: new Date()
         };
     }
 
-    componentDidMount() {  
-        console.log('FeedBabyTimer componentDidMount works!');
-        
-        fetch(`${config.API_ENDPOINT}/feedbabydata`)
-        .then(res => res.json())
-        .then( datalogs => {
-            this.setState({
-              date_created: datalogs[datalogs.length-1].date_created
-            });
-            console.log(this.state.date_created);
-        });   
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
     }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    // componentDidMount() {  
+    //     console.log('FeedBabyTimer componentDidMount works!');
+        
+    //     fetch(`${config.API_ENDPOINT}/feedbabydata`)
+    //     .then(res => res.json())
+    //     .then( datalogs => {
+    //         this.setState({
+    //           date_created: datalogs[datalogs.length-1].date_created
+    //         });
+    //         console.log(this.state.date_created);
+    //     });   
+    // }
+            // //this returns the correct time difference in milliseconds
+            // let curr = new Date();
+            // let prev = new Date(this.state.date_created); 
+            // let timer = curr - prev;
+            // // let h,m,s;
+            // let h = Math.floor(timer/1000/60/60);
+            // let m = Math.floor((timer/1000/60/60 - h)*60);
+            // let s = Math.floor(((timer/1000/60/60 - h)*60 - m)*60);
+    
+            // s < 10 ? s = `0${s}`: s = `${s}`
+            // m < 10 ? m = `0${m}`: m = `${m}`
+            // h < 10 ? h = `0${h}`: h = `${h}`
+    
+            // let timeSinceLastFeed = `${h}:${m}:${s}`;
+            
+            // console.log(timeSinceLastFeed);
 
 
     render() {
-        //shows the time of last baby feeding log
-        let lastFeedBabyTime = 
-        new Date(this.state.date_created); 
-
-        //this returns the correct timing
-        let curr = new Date();
-        let prev = new Date(this.state.date_created); 
-        let timer = curr - prev;
-        // let h,m,s;
-        let h = Math.floor(timer/1000/60/60);
-        let m = Math.floor((timer/1000/60/60 - h)*60);
-        let s = Math.floor(((timer/1000/60/60 - h)*60 - m)*60);
-
-        s < 10 ? s = `0${s}`: s = `${s}`
-        m < 10 ? m = `0${m}`: m = `${m}`
-        h < 10 ? h = `0${h}`: h = `${h}`
-
-        let timeSinceLastFeed = `${h}:${m}:${s}`;
-
-        console.log(timeSinceLastFeed);
-
-     
-        
-
-
-
         return (
             <div className="flex-container">
                 <p className='timer-text'>
@@ -58,7 +66,8 @@ class FeedBabyTimer extends React.Component {
                 </p>
 
                 <p className='timer'>
-                    {timeSinceLastFeed}
+                    {/* {timeSinceLastFeed} */}
+                    {this.state.date.toLocaleTimeString()}
                 </p>
             </div>
         ); 
