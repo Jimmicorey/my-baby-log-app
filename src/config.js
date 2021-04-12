35c3cc999c494f7cde5bf007285eab5db25b52e2
James Corey Coffelt <james.c.coffelt@gmail.com>
2021-04-11 21:25:02 -0500

    updated codebase

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 export default {
-  //LIVE SERVER AT THE FOLLOWING
-  API_ENDPOINT: 'https://my-baby-log-api.herokuapp.com/api/datalogs'
+  /*NOTE LIVE SERVER NO LONGER EXISTS! */
+
+  /*LIVE SERVER AT THE FOLLOWING*/
+
+  //API_ENDPOINT: 'https://my-baby-log-api.herokuapp.com/api/datalogs'
+
+  
+  API_ENDPOINT: 'http://localhost:8000/api/datalogs/'
 }
